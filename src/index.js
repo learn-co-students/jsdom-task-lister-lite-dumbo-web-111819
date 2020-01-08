7254f129a770ee5bd2296fc87c504bfbd10431d4
MohammedQasim <mqshaikh8@gmail.com>
2020-01-07 20:38:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded",function(){
+  const taskUl = document.querySelector('#tasks');
+  const taskForm = document.querySelector('#create-task-form');
+  // console.log(taskUl)
+  // console.log(taskForm)
+  taskForm.addEventListener("submit",function(e){
+    e.preventDefault()
+    //getting the input field through the name
+    //value = user input
+    //e exist inside the function
+    console.log(e)
+    console.log(e.target)
+    const userInput = e.target["new-task-description"].value
+    const newInput = document.createElement("li")
+    newInput.innerText = userInput
+    taskUl.append(newInput)
+  })
+
+
+
+
+
+
+  taskUl.addEventListener("click",function(e){
+    
+    // console.log(e.target.tagName)
+    if(e.target.tagName === "LI"){
+      
+      e.target.remove()
+    }
+  })
+
 });
