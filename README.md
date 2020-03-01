01f5af244178602f02bab7321defe3853fceee81
Soner Mezgitci <you@example.com>
2020-02-29 22:01:42 -0500

    Done.

--- README.md
+++ README.md
@@ -62,3 +62,26 @@ If you finish early, try to implement one or more of the following:
 
 
  
+
+
+
+
+
+ const taskButton = document.querySelector("#create-task-form")
+taskButton.addEventListener("submit", handleTaskFormSubmit)
+
+function handleTaskFormSubmit(event){
+    event.preventDefault()
+    // debugger  
+    // console.log('sup')
+    let taskWord = event.target.querySelector("#new-task-description").value
+    let tasks = document.querySelector("#tasks")
+    console.log(tasks)
+    const newLi= document.createElement("li")
+    newLi.innerText = taskWord
+    tasks.append(newLi)
+}
+// call or invoke()
+// function handleEmojiFormSubmit(event){
+//     event.preventDefault()
+//     const newEmoji = event.target["emoji"].value
--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
-const taskButton = document.querySelector("#create-task-form")
-taskButton.addEventListener("submit", handleTaskFormSubmit)
+const main =document.querySelector("#main-content")
+console.log(main)
 
-function handleTaskFormSubmit(event){
-    event.preventDefault()
-    // debugger  
-    // console.log('sup')
-    let taskWord = event.target.querySelector("#new-task-description").value
-    let tasks = document.querySelector("#tasks")
-    console.log(tasks)
-    const newLi= document.createElement("li")
-    newLi.innerText = taskWord
-    tasks.append(newLi)
-}
-// call or invoke()
-// function handleEmojiFormSubmit(event){
-//     event.preventDefault()
-//     const newEmoji = event.target["emoji"].value
+
+let taskForm = document.querySelector("#create-task-form")
+const taskList = document.querySelector("#tasks")
+
+
+taskForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    //have the user input
+    const newTask = document.querySelector("#new-task-description").value
+    
+//slap it on th DOM
+//Where i should put it ?
+
+
+taskList.innerHTML += `
+<li> ${newTask} 
+<button data-action= "delete" data-matt="PowerFULL" data-charlie="downfield">X for delete </button>
+</li>
+`
+
+// const taskItem = document.createElement("li")
+// taskItem.innerText = newTask
+// taskList.appendChild(taskItem)
+taskForm.reset();
+
+
+})
+taskList.addEventListener("click", function(e){
+    if (e.target.dataset.action ==="delete"){
+        e.target.parentElement.remove()
+    }
+    
+
+})
+
+
